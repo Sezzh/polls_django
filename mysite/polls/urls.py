aee5843b3a27afa6f68733360e0e7ec208476e8e
Jesús Cruz <sezzh@outlook.com>
2015-09-16 13:24:34 -0500

    parte 4 del tutorial de django antes de Generic views

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,8 +4,9 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name = 'index'),
-    url(r'^specifics/(?P<question_id>[0-9]+)/$', views.detail, name = 'detail'),
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name = 'detail'),
     url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name = 'results'),
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name = 'vote'),
-    url(r'^sezzh', views.sezzh, name = 'sezzh_pattern'),
+    url(r'^sezzh', views.sezzh, name = 'sezzh'),
+    url(r'^sezzh-form-send$', views.sezzh_form_send, name="sezzh_form_send"),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render
-from django.http import HttpResponse, Http404
-'''from django.template import RequestContext, loader'''
-''' mandando a llamar a las vistas: HttpResponse, RequestContext y loader o podemos cargar solo a shortcuts render '''
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect, HttpResponse, Http404
+from django.core.urlresolvers import reverse
 
-from .models import Question
+'''from django.template import RequestContext, loader'''
+''' mandando a llamar a las vistas: HttpResponse, RequestContext y loader o
+podemos cargar solo a shortcuts render '''
+
+from .models import Question, Choice
 
 # Create your views here.
 
@@ -11,6 +14,8 @@ from .models import Question
 def index(request):
     '''return HttpResponse("Hello, world. You're at the polls index.")'''
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    print latest_question_list
+    print type(latest_question_list)
     sezzhRights = 'This is a webpage made by sezzh'
     context = {
         'latest_question_list': latest_question_list,
@@ -31,13 +36,47 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s. "
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk = question_id)
+    context = {
+        'question': question,
+    }
+    return render(request, 'polls/results.html', context)
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    p = get_object_or_404(Question, pk = question_id)
+    try:
+        selected_choice = p.choice_set.get(pk = request.POST['choice'])
+        print "esto es selected_choice: " + str(selected_choice)
+        print p
+    except (KeyError, Choice.DoesNotExist):
+        #redisplay the question voting form
+        context = {
+            'question': p,
+            'error_message': "You didn't select a choice...",
+        }
+        return render(request, 'polls/detail.html', context)
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        #with POST data. This prevents data from being posted twice if a user
+        #hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
 
-def sezzh(request):
+
+################################################################################
+
+
+def sezzh(request, name = 'bebi', working = 'default'):
     ''' This is a example view function to see how it is works '''
-    print request
-    return HttpResponse('entraste a sezzh')
+    context = {
+        'title': 'Esto es una prueba de contextos',
+        'name': name,
+        'working': working,
+    }
+    return render(request, 'polls/sezzh.html', context)
+
+def sezzh_form_send(request):
+    name = request.POST['name']
+    working = 'funciono'
+    return HttpResponseRedirect(reverse('polls:sezzh', args = (name, working,)))
